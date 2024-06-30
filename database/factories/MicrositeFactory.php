a1127eee15f362cfb94dbb14b2701215f6c683b9
Marco Orozco <markuz1995@gmail.com>
2024-06-30 18:34:59 -0500

    test: add test for category and microsite (#13)
    Signed-off-by: Marco Orozco <markuz1995@gmail.com>

--- database/factories/MicrositeFactory.php
+++ database/factories/MicrositeFactory.php
@@ -2,13 +2,14 @@
 
 namespace Database\Factories;
 
+use App\Constants\Constants;
+use App\Domains\Microsite\Models\Microsite;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Microsite>
- */
 class MicrositeFactory extends Factory
 {
+    protected $model = Microsite::class;
+
     /**
      * Define the model's default state.
      *
@@ -16,8 +17,15 @@ class MicrositeFactory extends Factory
      */
     public function definition(): array
     {
+        $category = CategoryFactory::new()->create();
+
         return [
-            //
+            'name' => $this->faker->company,
+            'logo' => $this->faker->imageUrl(640, 480, 'business', true),
+            'category_id' => $category->id,
+            'currency' => Constants::MICROSITE_CURRENCY[0],
+            'payment_expiration' => $this->faker->dateTimeBetween('+1 month', '+1 year'),
+            'type' => $this->faker->randomElement(['invoice', 'subscription', 'donation']),
         ];
     }
 }
